441876a12d572f9c9b6ffdc31fb3e93eecfe5e8c
prakhar2407 <65302454+prakhar2407@users.noreply.github.com>
2021-11-30 17:06:08 +0530

    Delete NO_SQL_Assessment directory

--- NO_SQL_Assessment/Backend/src/routes/product.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-//const {  } = require('../controller/category');
-const { getProducts } = require("../controller/product");
-const router = express.Router();
-
-router.post("/product/getProducts", getProducts);
-
-module.exports = router;
